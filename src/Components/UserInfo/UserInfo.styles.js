d5ebc3fa04d60b1853219542524860c205bffa7b
Juan Urdaneta <juandurdanet@gmail.com>
2021-10-27 22:11:29 -0400

    Added bio and header styled component

--- src/Components/UserInfo/UserInfo.styles.js
+++ src/Components/UserInfo/UserInfo.styles.js
@@ -1,37 +1,35 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-    margin-top: 2%;
-    border-radius: 15px;
-    display: flex;
-    background: var(--lightBlue);
-    padding: 35px 5px 25px 15px;
-    width: 100%;
-    box-shadow: 1px 3px 2px 1px #101625;
-
-
-
-`
-
-
+  margin-top: 2%;
+  border-radius: 15px;
+  display: flex;
+  background: var(--lightBlue);
+  padding: 35px 15px 35px 15px;
+  width: 100%;
+  box-shadow: 1px 3px 2px 1px #101625;
+`;
 export const Content = styled.div`
 
+`;
+export const Header = styled.div`
 
-        h2, p{
-            display: inline;
-        }
+    img {
+        width: 120px;
+        border-radius: 60px;
+        float: left;
+        margin: auto 40px auto 30px;
+    }
 
-        p{
-            margin-left: 10rem;
-            font-size: .9rem;
-        }
 
-        img{
-            width: 120px;
-            border-radius: 60px;
-            float: left;
-            margin: auto 40px auto 30px;
-        }
-        
+  h2,
+  p {
+    display: inline;
+  }
 
-`
+  p {
+    margin-left: 9rem;
+    font-size: 0.9rem;
+    
+  }
+`;
--- src/Components/UserInfo/index.js
+++ src/Components/UserInfo/index.js
@@ -1,25 +1,28 @@
 import React from "react";
-import { Content, ProfilePic } from "../UserInfo/UserInfo.styles";
+import { Content, Header } from "../UserInfo/UserInfo.styles";
 
 // styles
 import { Wrapper } from "../UserInfo/UserInfo.styles";
 
 const UserInfo = ({ user }) => {
-    const options = {day: 'numeric' , year: 'numeric', month: 'short'}
-    var date = new Date(user.created_at);
+  const options = { day: "numeric", year: "numeric", month: "short" };
+  var date = new Date(user.created_at);
 
-    date = date.toLocaleDateString('en-US', options);
-
-
-    console.log(date)
+  date = date.toLocaleDateString("en-US", options);
 
   return (
     <Wrapper>
       <Content>
-        <img src={user.avatar_url} />
-        {user.name ?<h2> {user.name} </h2> : <h2>null</h2>}
-        <p>Joined {date}</p>
-        
+        <Header>
+          <img src={user.avatar_url} />
+          {user.name ? <h2> {user.name} </h2> : <h2>null</h2>}
+          <p>Joined {date}</p>
+        </Header>
+
+        <p style={{color:"#1b79e4"}}>@{user.login}</p>
+        {user.bio ? <p>{user.bio}</p> : <p>This profile has no bio.</p>}
+
+
       </Content>
     </Wrapper>
   );
